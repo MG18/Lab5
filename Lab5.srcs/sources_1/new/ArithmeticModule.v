9cbe9b9ec5a690e32762bde05899634ba368a2b5
Mehgul <mehgul@student.ethz.ch>
2023-04-27 20:35:07 +0200

    Added Aithmetic moduke

--- Lab5.srcs/sources_1/new/ArithmeticModule.v
+++ Lab5.srcs/sources_1/new/ArithmeticModule.v
@@ -20,6 +20,10 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ArithmeticModule(input[3:0] AluOp, input[31:0] A, input[31:0] B, output[31:0] out);
+module ArithmeticModule(input[3:0] AluOp, input[31:0] A, input[31:0] B, output[31:0] out); 
+Wire addOut;
+
+ThirtyTwoAdder(AluOp[2], A, B, addOut);
+SLT(addOut, AluOp[0], out);
 
 endmodule
